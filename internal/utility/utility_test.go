38f74beb861cbba974452e13175e3c861bf6727f
xanish <danishali7866@gmail.com>
2024-09-28 22:32:56 +0530

    add test for utility.PeerID

--- internal/utility/utility_test.go
+++ internal/utility/utility_test.go
@@ -5,6 +5,18 @@ import (
 	"testing"
 )
 
+func TestPeerID(t *testing.T) {
+	id, err := PeerID()
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+
+	zeroID := [20]byte{}
+	if id == zeroID {
+		t.Errorf("expected ID to be non-zero, but got zero ID")
+	}
+}
+
 func TestPieceExists(t *testing.T) {
 	bitfield := []byte{0b10110101, 0b01010000}
 	expected := []bool{true, false, true, true, false, true, false, true, false, true, false, true, false, false, false, false, false}
