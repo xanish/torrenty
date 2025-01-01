a131fcd536528903ad82ed4190c5f5ddd67c4732
xanish <danishali7866@gmail.com>
2025-01-01 16:30:10 +0530

    add piece worker logic

--- internal/torrent/piece_work.go
+++ internal/torrent/piece_work.go
@@ -1,7 +1,13 @@
 package torrent
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"math"
+
 	"github.com/xanish/torrenty/internal/peer"
+	"github.com/xanish/torrenty/internal/protocol"
 )
 
 type PieceWork struct {
@@ -28,5 +34,87 @@ func NewPieceWorker(id int, clientID, infoHash [20]byte, peer peer.Peer) PieceWo
 }
 
 func (pw PieceWorker) DoWork(jobs chan PieceWork, results chan<- PieceWork) error {
+	const downloadBlockSize uint32 = 16 * 1024
+
+	err := pw.peer.Connect(pw.clientID, pw.infoHash)
+	if err != nil {
+		return fmt.Errorf("failed to connect to peer %s: %v", pw.peer.String(), err)
+	}
+
+	for piece := range jobs {
+		// Peer does not have this piece so move on to find the next piece from
+		// this peer which we can download
+		if !pw.peer.HasPiece(piece.index) {
+			requeueWork(piece, jobs)
+			continue
+		}
+
+		// download piece block-by-block
+		numBlocks := uint32(math.Ceil(float64(piece.size) / float64(downloadBlockSize)))
+
+		var i uint32
+		for i = 0; i < numBlocks; i++ {
+			adjustedBlockSize := downloadBlockSize
+			if i == numBlocks-1 {
+				adjustedBlockSize = piece.size - ((numBlocks - 1) * downloadBlockSize)
+			}
+
+			err := pw.peer.Send(protocol.MessageConf{
+				Type:       protocol.MsgTypeRequest,
+				PieceIndex: piece.index,
+				Begin:      i * downloadBlockSize,
+				Length:     adjustedBlockSize,
+			})
+			if err != nil {
+				// Something went wrong while requesting for current piece, just
+				// add it to backlog and try later
+				requeueWork(piece, jobs)
+
+				break
+			}
+
+			piece.result, err = pw.peer.Read()
+			if err != nil {
+				requeueWork(piece, jobs)
+
+				// Break out of the loop here to allow worker to download the
+				// next available piece from this peer
+				break
+			}
+		}
+
+		// check piece integrity
+		hash := sha1.Sum(piece.result)
+		if !bytes.Equal(hash[:], piece.hash[:]) {
+			// Hash for the piece did not match so we requeue it and download
+			// it later
+			requeueWork(piece, jobs)
+			continue
+		} else {
+			results <- piece
+		}
+
+		// Inform the peer that we received the message
+		err = pw.peer.Send(protocol.MessageConf{Type: protocol.MsgTypeHave, PieceIndex: piece.index})
+		if err != nil {
+			return fmt.Errorf("failed to send message have to peer %s: %v", pw.peer.String(), err)
+		}
+	}
+
 	return nil
+}
+
+func requeueWork(work PieceWork, jobs chan<- PieceWork) bool {
+	isChanClosed := false
+	defer func() {
+		if recover() != nil {
+			isChanClosed = true
+		}
+	}()
+
+	// Reset the result payload
+	work.result = make([]byte, work.size)
+	jobs <- work
+
+	return isChanClosed
 }
