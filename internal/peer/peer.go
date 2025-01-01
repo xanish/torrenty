b0157e15b5d9f0cd06cf2f9eb97905c53366d78f
xanish <danishali7866@gmail.com>
2025-01-01 16:45:08 +0530

    add logic to peer for connect and parse message

--- internal/peer/peer.go
+++ internal/peer/peer.go
@@ -13,10 +13,12 @@ import (
 const peerNumBytes = 6 // 4 bytes for IP, 2 bytes for port
 
 type Peer struct {
-	ip       net.IP
-	port     uint16
-	conn     protocol.Connection
-	bitfield bitfield.Bitfield
+	ip           net.IP
+	port         uint16
+	conn         protocol.Connection
+	bitfield     bitfield.Bitfield
+	amChoked     bool
+	amInterested bool
 }
 
 func New(encodedPeers string) ([]Peer, error) {
@@ -40,7 +42,59 @@ func New(encodedPeers string) ([]Peer, error) {
 }
 
 func (p *Peer) Connect(peerID, infoHash [20]byte) error {
-	return nil
+	conn, err := protocol.NewConnection(p.String(), peerID, infoHash)
+	if err != nil {
+		return err
+	}
+	p.conn = *conn
+
+	msg, err := p.conn.ReadMessage()
+	if err != nil {
+		return err
+	}
+
+	_, err = p.parseMessage(msg)
+
+	return err
+}
+
+func (p *Peer) parseMessage(msg *protocol.Message) (*protocol.Piece, error) {
+	switch msg.Type {
+	case protocol.MsgTypeChoke:
+		p.amChoked = true
+	case protocol.MsgTypeUnChoke:
+		p.amChoked = false
+	case protocol.MsgTypeInterested:
+		p.amInterested = true
+	case protocol.MsgTypeNotInterested:
+		p.amInterested = false
+	case protocol.MsgTypeHave:
+		index, err := protocol.ParseHave(msg)
+		if err != nil {
+			return nil, err
+		}
+		p.bitfield.SetPiece(index)
+	case protocol.MsgTypeBitfield:
+		p.bitfield = msg.Payload
+	case protocol.MsgTypeRequest:
+		_, _, _, err := protocol.ParseRequest(msg)
+		if err != nil {
+			return nil, err
+		}
+	case protocol.MsgTypePiece:
+		piece, err := protocol.ParsePiece(msg)
+		if err != nil {
+			return nil, err
+		}
+
+		return piece, nil
+	case protocol.MsgTypeCancel:
+		// todo: add logic
+	case protocol.MsgTypePort:
+		// todo: add logic
+	}
+
+	return nil, nil
 }
 
 func (p *Peer) Close() error {
