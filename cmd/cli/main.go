f4f233dc2527c31e9a2b46aedd0e682c39fd4376
xanish <danishali7866@gmail.com>
2025-02-16 15:09:55 +0530

    Refactor main.go to break down functionality

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -11,6 +11,19 @@ import (
 	"github.com/xanish/torrenty/internal/torrent"
 )
 
+func main() {
+	config := parseFlags()
+	if err := validateFlags(config); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := processDownload(config); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 type DownloadFlags struct {
 	TorrentFile string
 	MagnetLink  string
@@ -37,8 +50,10 @@ func validateFlags(config *DownloadFlags) error {
 		return errors.New("you must provide either a torrent file or a magnet link URL")
 	}
 
-	if _, err := os.Stat(config.TorrentFile); os.IsNotExist(err) {
-		return fmt.Errorf("torrent file %s does not exist", config.TorrentFile)
+	if config.TorrentFile != "" {
+		if _, err := os.Stat(config.TorrentFile); os.IsNotExist(err) {
+			return fmt.Errorf("torrent file %s does not exist", config.TorrentFile)
+		}
 	}
 
 	if config.Destination == "" {
@@ -53,52 +68,59 @@ func validateFlags(config *DownloadFlags) error {
 }
 
 func processDownload(config *DownloadFlags) error {
-	if config.TorrentFile != "" {
-		file, err := os.Open(config.TorrentFile)
-		if err != nil {
-			return fmt.Errorf("failed to read torrent file %s: %s", config.TorrentFile, err.Error())
-		}
-		defer file.Close()
-
-		t, err := torrent.FromFile(file)
-		if err != nil {
-			return fmt.Errorf("failed to read torrent file %s: %s", config.TorrentFile, err.Error())
-		}
-
-		config.Destination = filepath.Clean(config.Destination)
-		if !strings.HasSuffix(config.Destination, string(filepath.Separator)) {
-			config.Destination += string(filepath.Separator)
-		}
-
-		out, err := os.Create(config.Destination + t.Name())
-		if err != nil {
-			return fmt.Errorf("could not create output file %s: %w", config.Destination, err)
-		}
-		defer func(out *os.File) {
-			_ = out.Close()
-		}(out)
-
-		err = out.Truncate(int64(t.Size()))
-		if err != nil {
-			return fmt.Errorf("could not allocate %d bytes for file %s: %w", t.Size(), file, err)
-		}
-
-		return t.Download(out)
+	t, err := createTorrent(config)
+	if err != nil {
+		return err
 	}
 
-	// todo: actual download logic for magnet link here
-	return nil
+	dest := filepath.Clean(config.Destination)
+	if !strings.HasSuffix(config.Destination, string(filepath.Separator)) {
+		dest += string(filepath.Separator)
+	}
+
+	out, err := os.Create(dest + t.Name())
+	if err != nil {
+		return fmt.Errorf("could not create output file %s: %w", dest, err)
+	}
+	defer out.Close()
+
+	if err = out.Truncate(int64(t.Size())); err != nil {
+		return fmt.Errorf("could not allocate %d bytes for file %s: %w", t.Size(), out.Name(), err)
+	}
+
+	return t.Download(out)
 }
 
-func main() {
-	config := parseFlags()
-	if err := validateFlags(config); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+func createTorrent(config *DownloadFlags) (*torrent.Torrent, error) {
+	if config.TorrentFile != "" {
+		return torrentFromFile(config)
+	} else if config.MagnetLink != "" {
+		return torrentFromMagnet(config)
 	}
 
-	if err := processDownload(config); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	return nil, errors.New("no torrent file or magnet link provided")
+}
+
+func torrentFromFile(config *DownloadFlags) (*torrent.Torrent, error) {
+	file, err := os.Open(config.TorrentFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read torrent file %s: %s", config.TorrentFile, err.Error())
 	}
+	defer file.Close()
+
+	t, err := torrent.FromFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read torrent file %s: %s", config.TorrentFile, err.Error())
+	}
+
+	return t, nil
+}
+
+func torrentFromMagnet(config *DownloadFlags) (*torrent.Torrent, error) {
+	t, err := torrent.FromMagnet(config.MagnetLink)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create torrent from magnet %s: %s", config.MagnetLink, err.Error())
+	}
+
+	return t, nil
 }
